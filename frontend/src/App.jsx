48b0335e67ba66f87e4a8e58ab124fa8ae8aa217
Murali Krishna <krishnamurali.barla@sasi.ac.in>
2024-10-22 15:06:45 +0530

    added folders by mk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import HeroSection from './components/HeroSection';
 
 const App = () => {
   return (
     <div>
       Hello!
+      <HeroSection />
     </div>
   )
 }
